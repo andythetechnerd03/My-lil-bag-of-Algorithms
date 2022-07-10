d7e047b6989706c01829cf6864a94f3736fc9615
Đinh Ngọc Ân <101492362+andythetechnerd03@users.noreply.github.com>
2022-07-10 20:15:08 +0700

    Create gcd_as_linear_combination.py

---
+++ GCD as Linear Combination/gcd_as_linear_combination.py
@@ -0,0 +1,31 @@
+# Write GCD as linear combination of a and b (a>b) (Extended Euclidean algorithm)
+
+
+def find_quotients(a, b):
+    x = a
+    y = b
+    quotients = []
+    while y != 0:
+        quotients.append(x // y)
+        r = x % y
+        x = y
+        y = r
+    return quotients
+
+
+def linear_combination(a, b):
+    s = [1, 0]
+    t = [0, 1]
+    quotients = find_quotients(a, b)
+    for i in range(2, len(quotients) + 1):
+        s.append(s[i - 2] - quotients[i - 2] * s[i - 1])
+        t.append(t[i - 2] - quotients[i - 2] * t[i - 1])
+    return [s[-1], t[-1]]
+
+
+if __name__ == "__main__":
+    a, b = list(map(int, input().split()))
+    a = max(a, b)
+    b = min(a, b)
+    x, y = linear_combination(a, b)
+    print(x, y)
